9b832768bcd35b41a6e571e5f52cdf3688c313f7
CSE15L Student <studentemail@email.com>
2014-04-07 17:55:27 -0700

    Hello uses Greeter

--- lib/Hello.java
+++ lib/Hello.java
@@ -3,13 +3,13 @@ public class Hello
     public static void main( String argv[] )
     {
         // Default is "World"
-        // Author: CSE15L Student (studentemail@email.com)
         String name = "World";
         if ( argv.length != 0 )
         {
             name = argv[0];
         }
 
-        System.out.println( "Hello, " + name + "!" );
+        Greeter greeter = new Greeter(name);
+        System.out.println(greeter.greet());
     }
 }
